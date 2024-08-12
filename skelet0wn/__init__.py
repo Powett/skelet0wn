138b038997e980eab0de1fbf73ca9270507b08eb
Powett <66013658+Powett@users.noreply.github.com>
2024-08-12 16:41:36 +0200

    Skull info

--- skelet0wn/__init__.py
+++ skelet0wn/__init__.py
@@ -1,4 +1,5 @@
 """This module contains the `skelet0wn` framework.
+`Skull` does not yet have an explicit class definition: it simply refers to a MongoDB Database object, used as central database for a running workflow instance.
 """
 
 __version__ = "1.0.1-dev"
